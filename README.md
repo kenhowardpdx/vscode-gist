b9041dcbdbe1abfced9b142d687ba9160059cd1b
David Bankier <dbankier@hotmail.com>
2015-12-02 10:18:27 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# vscode-gist
+
+> Extension for Visual Studio Code to create gists. They can be anonoymous, private or public and can be created from selection or full file.
+
+## Installation
+
+*This is pending the ms marketplace validation.*
+
+Press <kbd>F1</kbd> and narrow down the list commands by typing `extension`. Pick `Extensions: Install Extension`.
+Select the `Gist Extension` extension from the list
+
+##Install Manual
+
+**Mac & Linux**
+```sh
+cd $HOME/.vscode/extensions
+git clone https://github.com/dbankier/vscode-gist.git
+cd vscode-gist
+npm install
+```
+
+**Windows**
+```
+cd %USERPROFILE%\.vscode\extensions
+git clone https://github.com/dbankier/vscode-gist.git
+cd vscode-gist
+npm install
+```
+
+## Usage
+
+**Command**
+
+Press <kbd>F1</kbd> and type `Gist` and then select which type of gist you want to created.
+
+You will be prompted for your git credentials and gist description if needed.
+
+**Keybord Shortcut**
+
+None at this stage - but you can define your own.
+
+## Future
+Other gist functions: list, view, edit, import, etc.
+
+## License
+
+MIT © [David Bankier @dbankier](https://github.com/dbankier)
---
+++ extension.js
@@ -0,0 +1,105 @@
+var vscode = require('vscode');
+var shell = require('shelljs');
+var Promise = require('bluebird');
+var request = Promise.promisify(require("request"));
+var open = require('open');
+var window = vscode.window;
+
+var PRIVATE = 0;
+var PUBLIC = 1;
+var ANONYMOUS = 2;
+
+function activate(context) {
+  context.subscriptions.push(vscode.commands.registerCommand('extension.privateGist', createGist.bind(undefined, PRIVATE)));
+  context.subscriptions.push(vscode.commands.registerCommand('extension.publicGist', createGist.bind(undefined, PUBLIC)));
+  context.subscriptions.push(vscode.commands.registerCommand('extension.anonymousGist', createGist.bind(undefined, ANONYMOUS)));
+}
+
+function getCredentials() {
+  var user
+  return getUser().then(function (u) {
+    user = u;
+    return getPass()
+  })
+    .then(function (p) {
+      return Promise.resolve({
+        user: user,
+        pass: p,
+        sendImmediately: true
+      })
+    })
+}
+function getUser() {
+  var user_command = shell.exec("git config --get github.user")
+  if (user_command.code !== 0 || !user_command.output.trim()) {
+    return vscode.window.showInputBox({ prompt: "Enter your github username" })
+  } else {
+    return Promise.resolve(user_command.output.trim());
+  }
+}
+function getPass() {
+  var password_command = shell.exec("git config --get github.password")
+  if (password_command.code !== 0 || !password_command.output.trim()) {
+    return vscode.window.showInputBox({ prompt: "Enter your github password", password: true })
+  } else {
+    return Promise.resolve(password_command.output.trim());
+  }
+}
+function createGist(type) {
+  var auth;
+  var editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return vscode.window.showErrorMessage("First open a file");
+  }
+
+  var selection = editor.selection;
+  var text_content = editor.document.getText(selection.isEmpty ? undefined : selection);
+  if (!shell.which('git')) {
+    return vscode.window.showErrorMessage("Sorry, git must be installed.");
+  }
+  var promise = type !== ANONYMOUS ? getCredentials() : Promise.resolve();
+  return promise
+    .then(function (_auth) {
+      auth = _auth;
+      return vscode.window.showInputBox({ prompt: "Enter the gist description." })
+    })
+    .then(function (description) {
+      var api_command = shell.exec("git config --get github.password")
+      var api;
+      if (api_command.code !== 0 || !api_command.output.trim()) {
+        api = "https://api.github.com"
+      } else {
+        api = api_command.output.trim();
+      }
+      api += "/gists";
+
+      var options = {
+        method: 'POST',
+        uri: api,
+        json: true,
+        headers: {
+          "User-Agent": "VSCode-Gist-Extention"
+        },
+        body: {
+          description: description,
+          public: type !== PRIVATE,
+          files: {}
+        }
+      };
+      options.body.files[editor.document.fileName || "untitled.txt"] = {
+        content: text_content
+      }
+      if (type !== ANONYMOUS) {
+        options.auth = auth;
+      }
+      return request(options);
+    })
+    .then(function (res) {
+      var page = res.body.html_url
+      if (!page) {
+        return vscode.window.showErrorMessage(res.body && res.body.message ? res.body.message : "Could not create gist");
+      }
+      open(page);
+    })
+}
+exports.activate = activate;
