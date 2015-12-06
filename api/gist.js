2a63517747b21ebc7ab1ad9e969a7d6e215f1e92
David Bankier <dbankier@hotmail.com>
2015-12-06 18:21:10 +1100

    refactored again

--- api/gist.js
+++ api/gist.js
@@ -1,9 +1,6 @@
 var vscode = require('vscode');
 var Promise = require('bluebird');
 var request = Promise.promisify(require("request"));
-var open = require('open');
-var window = vscode.window;
-var path = require("path");
 var auth = require("./auth");
 var api = "https://api.github.com";
 
@@ -11,56 +8,41 @@ exports.PRIVATE = 0;
 exports.PUBLIC = 1;
 exports.ANONYMOUS = 2;
 
+
 function send(method, path, auth_type, body) {
   var oauth = auth.getToken();
   var promise = auth_type !== exports.ANONYMOUS && !oauth ? auth.getCredentials() : Promise.resolve();
   return promise.then(function (creds) {
-      console.log(creds);
-      var options = {
-        method: method,
-        uri: api + path,
-        json: true,
-        headers: {
-          "User-Agent": "VSCode-Gist-Extention"
-        },
-        body: body
-      };
-      if (auth_type !== exports.ANONYMOUS) {
-        if (oauth) {
-          options.headers["Authorization"] = "token " + oauth;
-        } else {
-          options.auth = creds;
-        }
+    console.log(creds);
+    var options = {
+      method: method,
+      uri: api + path,
+      json: true,
+      headers: {
+        "User-Agent": "VSCode-Gist-Extention"
+      },
+      body: body
+    };
+    if (auth_type !== exports.ANONYMOUS) {
+      if (oauth) {
+        options.headers["Authorization"] = "token " + oauth;
+      } else {
+        options.auth = creds;
       }
-      return request(options);
-    })
+    }
+    return request(options);
+  })
 }
 
-exports.createGist = function createGist(type) {
-  var editor = vscode.window.activeTextEditor;
-  if (!editor) {
-    return vscode.window.showErrorMessage("First open a file");
+exports.create = function (type, description, file_name, text_content) {
+  var body = {
+    description: description,
+    public: type !== exports.PRIVATE,
+    files: {}
   }
-  var selection = editor.selection;
-  var text_content = editor.document.getText(selection.isEmpty ? undefined : selection);
-  return vscode.window.showInputBox({ prompt: "Enter the gist description." })
-    .then(function (description) {
-      var body = {
-        description: description,
-        public: type !== exports.PRIVATE,
-        files: {}
-      }
-      body.files[path.basename(editor.document.fileName || "untitled.txt")] = {
-        content: text_content
-      }
-      return send("POST", "/gists", type, body);
-    })
-    .then(function (res) {
-      var page = res.body.html_url
-      if (!page) {
-        return vscode.window.showErrorMessage(res.body && res.body.message ? res.body.message : "Could not create gist");
-      }
-      open(page);
-    })
-}
+  body.files[file_name] = {
+    content: text_content
+  }
+  return send("POST", "/gists", type, body);
+};
 
