9e86bdb6caeb49a410f60ceb8d266d6bbf865005
Ken Howard <kenh@csgpro.com>
2017-03-05 16:55:25 -0800

    refactor(extension): Remove code

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,6 @@
-import * as commands from './commands';
-import Gist from './api/gist';
-import * as vscode from 'vscode';
 
 function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(vscode.commands.registerCommand('extension.privateGist', commands.createGist.bind(undefined, Gist.Type.PRIVATE)));
-  context.subscriptions.push(vscode.commands.registerCommand('extension.publicGist', commands.createGist.bind(undefined, Gist.Type.PUBLIC)));
-  context.subscriptions.push(vscode.commands.registerCommand('extension.anonymousGist', commands.createGist.bind(undefined, Gist.Type.ANONYMOUS)));
-  context.subscriptions.push(vscode.commands.registerCommand('extension.openGist', commands.openGist));
-  context.subscriptions.push(vscode.commands.registerCommand('extension.openStarredGist', commands.openStarredGist));
-  context.subscriptions.push(vscode.commands.registerCommand('extension.deleteCurrentGist', commands.deleteCurrentGist));
-  context.subscriptions.push(vscode.commands.registerCommand('extension.removeFileFromGist', commands.removeFileFromGist));
-  context.subscriptions.push(vscode.commands.registerCommand('extension.addNewFileToGist', commands.addNewFileToGist));
-  context.subscriptions.push(vscode.commands.registerCommand('extension.changeGistDescription', commands.changeGistDescription));
-  context.subscriptions.push(vscode.commands.registerCommand('extension.openGistInBrowser', commands.openGistInBrowser));
 
-  vscode.workspace.onDidSaveTextDocument(commands.onSave);
 }
 
 exports.activate = activate;
--- test/api/auth.test.ts
+++
@@ -1,8 +0,0 @@
-import * as assert from 'assert';
-import { getToken } from '../../src/api/auth';
-
-suite('Auth', () => {
-  test('getToken returns string', () => {
-    assert.equal(typeof getToken(), 'string');
-  });
-});
