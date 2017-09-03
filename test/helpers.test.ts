ec292817b8cff8f81e317c95f10a94fd5b2fcb22
Ken Howard <ken@simplicatedweb.com>
2017-09-02 19:44:44 -0700

    test(insertText): refactor test to properly setup and teardown

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -1,19 +1,21 @@
 import * as assert from 'assert';
 import * as td from 'testdouble';
 
-import * as vscode from '../src/modules/vscode';
-
-const applyEdit = td.function('applyEdit');
-td.replace(vscode, 'workspace', { applyEdit });
-td.replace(vscode, 'Range', td.constructor());
-td.replace(vscode, 'TextEdit', td.constructor());
-td.replace(vscode, 'WorkspaceEdit', td.constructor(class WorkspaceEdit { set(uri: string, edits: any[]) {}}));
-td.when(applyEdit(td.matchers.anything())).thenResolve(true);
-
-import { insertText } from '../src/helpers';
+let insertText;
 
 suite('Helpers Tests', () => {
   suite('insertText test', () => {
+    suiteSetup(() => {
+      const vscode = require('../src/modules/vscode');
+      const applyEdit = td.function('applyEdit');
+      td.replace(vscode, 'workspace', { applyEdit });
+      td.replace(vscode, 'Range', td.constructor());
+      td.replace(vscode, 'TextEdit', td.constructor());
+      td.replace(vscode, 'WorkspaceEdit', td.constructor(class WorkspaceEdit { set(uri: string, edits: any[]) {}}));
+      td.when(applyEdit(td.matchers.anything())).thenResolve(true);
+      insertText = require('../src/helpers').insertText;
+    });
+    suiteTeardown(() => td.reset());
     test('should apply edit to given editor', (done) => {
       const editor: any = { document: { uri: '' }, selection: { start: 0, end: 0 }};
       insertText(editor, 'foo').then(result => {
